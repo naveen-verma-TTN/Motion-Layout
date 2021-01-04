5c252bb8d7b45a48dd4ff6654b0ca3f94354edaf
naveen.verma <naveen.verma@tothenew.com>
2021-01-04 14:16:21 +0530

    Motion layout demo first commit

---
+++ app/src/main/java/com/example/motion_layout_demo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.motion_layout_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_main)
+    }
+}
